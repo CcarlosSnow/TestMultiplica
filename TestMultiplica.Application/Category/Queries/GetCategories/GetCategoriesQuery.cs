5a25b74e3d63798c48424f35e53294b7b8c1f79e
Juan Carlos Bazán Ochoa <ccarlos_snow@hotmail.com>
2017-12-20 22:47:25 -0500

    Add project files.

---
+++ TestMultiplica.Application/Category/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace TestMultiplica.Application.Category.Queries.GetCategories
+{
+    public class GetCategoriesQuery : IGetCategoriesQuery
+    {
+        private readonly IMapper _Mapper;
+
+        public GetCategoriesQuery(IMapper Mapper)
+        {
+            _Mapper = Mapper;
+        }
+        public IEnumerable<GetCategoriesModel> Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
